76831e58789ba66e611f020edc7ba5e5d5a126e7
Daichi Kanasugi <dkanasugi13@gmail.com>
2018-04-30 15:11:23 -0700

    update

---
+++ skate_dic.py
@@ -0,0 +1,15 @@
+#Daichi Kanasugi
+#CST 205
+#Skateboard dictionary
+#This python file is about video files in a dictionary format
+
+video_info = {
+    {
+        "id_1" : "01_nollie_180_switch_5-0",
+        "tags" : ["nollie", "180", "switch"]
+    },
+
+    {
+        "id_1" : ""
+    }
+}
