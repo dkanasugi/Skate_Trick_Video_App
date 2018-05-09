e9b17ff7e039a51dfb25ccfc4f4c0a7fec10e9ac
Daichi Kanasugi <dkanasugi13@gmail.com>
2018-05-09 15:29:24 -0700

    updated dictionary

--- skate_dic.py
+++ skate_dic.py
@@ -10,6 +10,62 @@ video_info = {
     },
 
     {
-        "id_1" : ""
+        "id_1" : "02_backside_blunt_side",
+        "tags" : ["backside", "blunt", "side"]
+    },
+
+    {
+        "id_1" : "04_nollie_frontside_crook",
+        "tags" : ["nollie", "frontside", "crook"]
+    },
+
+    {
+        "id_1" : "05_backside_nose_grind_front_180_out",
+        "tags" : ["backside", "nose", "grind", "front", "180", "out"]
+    },
+
+    {
+        "id_1" : "06_switch_backside_board_slide",
+        "tags" : ["switch", "backside", "board", "slide"]
+    },
+
+    {
+        "id_1" : "07_switch_frontside_5-0",
+        "tags" : ["switch", "frontside", "5-0"]
+    },
+
+    {
+        "id_1" : "08_switch_backside_bluntside",
+        "tags" : ["switch", "backside", "bluntside"]
+    },
+
+    {
+        "id_1" : "09_backside_nose_grind",
+        "tags" : ["backside", "nose", "grind"]
+    },
+
+    {
+        "id_1" : "10_frontside_bluntslide",
+        "tags" : ["frontside", "bluntside"]
+    },
+
+    {
+        "id_1" : "11_backside_tailslide",
+        "tags" : ["backside", "tailslide"]
+    },
+
+    {
+        "id_1" : "12_nollie_lipslide",
+        "tags" : ["nollie", "lipslide"]
+    },
+
+    {
+        "id_1" : "13_backside_nose_grind_backside_180_out",
+        "tags" : ["backside", "nose", "grind", "backside", "180", "out"]
+    },
+
+    {
+        "id_1" : "14_fakie_backside_lipside",
+        "tags" : ["fakie", "backside" "lipside"]
     }
 }
